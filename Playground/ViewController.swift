e7a62f4690adc8e682a197c9b2c8ae6faf7c3ad8
jenny0714 <jennylin0714@gmail.com>
2021-03-04 12:44:21 +0800

    create textfield and button

--- Playground/ViewController.swift
+++ Playground/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var textField1: UITextField!
+    @IBAction func showMessage(sender: UIButton) {
+    
+        let alertController = UIAlertController(title: "Hi your answer is", message: textField1.text, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
 }
 
